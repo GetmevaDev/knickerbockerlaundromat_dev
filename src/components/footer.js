e872eacb21833367c5bc143836c7ba773ca96955
GetmevaDev <devteam@getmeva.com>
2021-05-19 10:51:38 +0500

    add link

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import FooterMenu from "./FooterMenu/FooterMenu"
 import Logo from "./Logo/Logo"
 import SocialMedia from "./SocialMedia/SocialMedia"
 import MoveUp from "../images/24px-move-up.png"
+import LogoM from "../images/221.svg"
 import {animateScroll as scroll} from "react-scroll"
 
 
@@ -20,8 +21,13 @@ const Footer = () => {
         marginTop: `37.68px`,
         background: `#2676BC`,
       }}>
-        <div className="container row" >
+        <div className={`container row jc-sb`} >
           <p className={`text`}>©Copyright 2020. All rights reserved</p>
+          <div className={`manufacturer row`}>
+            <img src={LogoM} alt="" />
+                <p>Web Design & Digital Marketing <br/>
+                  by <a href="https://robertgerov.com">Robert Gerov Media</a></p>
+          </div>
           <a className={`scrollToTop`} href="#" onClick={() => scroll.scrollToTop()}>
             <img src={MoveUp} alt="" />
           </a>
