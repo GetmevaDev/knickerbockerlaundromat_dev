23b3a3327dbbcd92b27d5225e4d5c7c9b7744333
Arsen <arsen@getmeva.com>
2023-08-17 17:13:22 +0500

    fix title

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        // apiURL: "https://cms-knick-dev-71b6bbdd3892.herokuapp.com",
-        apiURL: "http://localhost:1337",
+        apiURL: "https://cms-knick-dev-71b6bbdd3892.herokuapp.com",
+        // apiURL: "http://localhost:1337",
         // apiURL: process.env.DEPLOY_URL
         //   ? "https://cms-knick.herokuapp.com"
         //   : "http://localhost:1337",
