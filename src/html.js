75072da8a03733080c5e849d4d3c886f2d3f320a
Arsen <arsen@getmeva.com>
2023-09-08 15:15:40 +0500

    add google script

--- src/html.js
+++ src/html.js
@@ -7,17 +7,28 @@ export default function HTML(props) {
     <head>
 
 
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-2B2814NJX5"></script>
+
+      {/*<script async src="https://www.googletagmanager.com/gtag/js?id=G-2B2814NJX5"></script>*/}
+      {/*<script*/}
+      {/*  dangerouslySetInnerHTML={{*/}
+      {/*    __html: `window.dataLayer = window.dataLayer || [];*/}
+      {/*  function gtag(){dataLayer.push(arguments);}*/}
+      {/*  gtag('js', new Date());*/}
+
+      {/*  gtag('config', 'G-2B2814NJX5');`*/}
+      {/*  }}*/}
+      {/*/>*/}
+
+
       <script
         dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-2B2814NJX5');`
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5HNMP3KJ');`
         }}
       />
-
 
       <meta charSet="utf-8" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
@@ -28,7 +39,14 @@ export default function HTML(props) {
       {props.headComponents}
     </head>
     <body {...props.bodyAttributes}>
+    <noscript
+      dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HNMP3KJ"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+      }}
+    />
     {props.preBodyComponents}
+
     <div
       key={`body`}
       id="___gatsby"
