5cfead97f6b4d0090cbcabf3e626c77f8c94c9e0
Shakhboz Farkhodov <shaxbozfarhodov@gmail.com>
2024-03-11 12:24:18 +0500

    video added to home page

--- src/components/homePageComponents/sectionHeader/SectionHeader.js
+++ src/components/homePageComponents/sectionHeader/SectionHeader.js
@@ -1,53 +1,61 @@
 import React from "react"
-import {graphql, useStaticQuery, Link} from "gatsby"
-import BackgroundImage from "gatsby-background-image"
+import { graphql, useStaticQuery } from "gatsby"
+
+import homePageVideo from "../../../images/home-page-video.mp4"
 
 import classes from "./sectionHeader.module.scss"
 
 const SectionHeader = () => {
-
   const data = useStaticQuery(graphql`
-      {
-          strapiHomePage {
-              
-                  Pretitle
-                  Title
-                  Subtitle
-                  Background_SectionHeader {
-                      alternativeText
-                      url
-                  }
-              
-          }
+    {
+      strapiHomePage {
+        Pretitle
+        Title
+        Subtitle
+        Background_SectionHeader {
+          alternativeText
+          url
+        }
       }
+    }
   `)
 
-  return(
-
-
-          <section className={classes.sectionHeader}
-                   style={{background:  `#1a7ce2 url("${data.strapiHomePage.Background_SectionHeader[0].url}")`}}>
-
-            {/*<BackgroundImage*/}
-            {/*  Tag="section"*/}
-            {/*  className={classes.sectionHeader}*/}
-            {/*  fluid={data.strapiHomePage.Background_SectionHeader && data.strapiHomePage.Background_SectionHeader !== null ?*/}
-            {/*    data.strapiHomePage.Background_SectionHeader.childImageSharp.fluid :*/}
-            {/*    null}*/}
-            {/*  backgroundColor={`#2f89f3`}*/}
-            {/*>*/}
-            <div className={`container ${classes.row}`}>
-
-              {data.strapiHomePage.Pretitle && data.strapiHomePage.Pretitle !== null ? (<h3>{data.strapiHomePage.Pretitle}</h3>) : null}
-              {data.strapiHomePage.Title && data.strapiHomePage.Title !== null ? (<h2>{data.strapiHomePage.Title}</h2>) : null}
-              {data.strapiHomePage.Subtitle && data.strapiHomePage.Subtitle !== null ? (<h4>{data.strapiHomePage.Subtitle}</h4>) : null}
-
-
-            </div>
-          </section>
-
-
-
+  return (
+    <section
+      className={classes.sectionHeader}
+      style={{
+        background: `#1a7ce2 url("${data.strapiHomePage.Background_SectionHeader[0].url}")`,
+      }}
+    >
+      {/*<BackgroundImage*/}
+      {/*  Tag="section"*/}
+      {/*  className={classes.sectionHeader}*/}
+      {/*  fluid={data.strapiHomePage.Background_SectionHeader && data.strapiHomePage.Background_SectionHeader !== null ?*/}
+      {/*    data.strapiHomePage.Background_SectionHeader.childImageSharp.fluid :*/}
+      {/*    null}*/}
+      {/*  backgroundColor={`#2f89f3`}*/}
+      {/*>*/}
+      <div className={`container ${classes.row}`}>
+        <div className={classes.left}>
+          <video className={classes.video} controls>
+            <source src={homePageVideo} type="video/mp4" />
+          </video>
+        </div>
+        <div className={classes.right}>
+          {data.strapiHomePage.Pretitle &&
+          data.strapiHomePage.Pretitle !== null ? (
+            <h3>{data.strapiHomePage.Pretitle}</h3>
+          ) : null}
+          {data.strapiHomePage.Title && data.strapiHomePage.Title !== null ? (
+            <h2>{data.strapiHomePage.Title}</h2>
+          ) : null}
+          {data.strapiHomePage.Subtitle &&
+          data.strapiHomePage.Subtitle !== null ? (
+            <h4>{data.strapiHomePage.Subtitle}</h4>
+          ) : null}
+        </div>
+      </div>
+    </section>
   )
 }
 
