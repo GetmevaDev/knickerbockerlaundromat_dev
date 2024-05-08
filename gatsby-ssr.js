cc5aff16158ed80612752bc597777a989358cc46
Shakhboz Farkhodov <shaxbozfarhodov@gmail.com>
2024-05-08 14:36:36 +0500

    google scripts added

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,26 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react")
+
+exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
+  setHeadComponents([
+    <script
+      key="gtm"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5HNMP3KJ');`,
+      }}
+    />,
+  ])
+
+  setPostBodyComponents([
+    <noscript key="gtm-noscript">
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-5HNMP3KJ"
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      />
+    </noscript>,
+  ])
+}
